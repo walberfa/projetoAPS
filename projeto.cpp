607f5fa6a059428f3696aa4c118651fa5249a238
Walber Florêncio <59696629+walberfa@users.noreply.github.com>
2021-12-24 02:06:37 -0300

    Adicionado cadastro de alunos e professores

--- projeto.cpp
+++ projeto.cpp
@@ -4,7 +4,23 @@
 
 int opcao = 0;
 
-void FazerLogin(){
+struct pessoa{
+    char nome[20],cpf[11],endereco[20],email[20],telefone[11];
+    int dia,mes,ano;
+};
+
+struct aluno{
+    char matricula[10],curso[10];
+    int semestre;
+};
+
+struct prof{
+    char siape[10];
+    int status;
+};
+
+
+void TelaLogin(){
 
     char login[15] = "wbr42";
     char login1[15];
@@ -25,7 +41,9 @@ void FazerLogin(){
         }
         else
             printf("\n\nDADOS INVALIDOS!\n\n");    
-    }
+    }    
+
+
 
 }
 
@@ -37,7 +55,7 @@ void MostrarMenu(){
 
     printf("1) - CADASTRO DE ALUNO\n");
     printf("2) - CADASTRO DE PROFESSOR\n");
-    printf("3) - CADASTRAR TCC\n\n");    
+    printf("3) - CADASTRO DE TCC\n\n");    
     printf("DIGITE A OPÇÃO DESEJADA: ");
     scanf("%d", &opcao);
 
@@ -45,19 +63,108 @@ void MostrarMenu(){
 
 }
 
+/*Criacao dos arquivos*/
+    FILE *aluno,*professor,*tcc;
+
 void CadastroAlunos(){
 
-    printf("\n\nAdicione os dados do Aluno\n");
+    struct pessoa alunos;
+    struct aluno aluno1;
+
+    /*Arquivos onde serÃ£o armazenados os dados*/
+    aluno = fopen("aluno.txt", "a+");
+
+    /*Realizar o cadastro do cliente*/
+
+    printf("Entre com o nome: ");
+    scanf("%s",alunos.nome);
+    
+    while (strcmp(alunos.nome,"FIM")){
+        
+        printf("Entre com o CPF: ");
+        scanf("%s",alunos.cpf);
+        printf("Entre com o numero de matricula: ");
+        scanf("%s",aluno1.matricula);
+        printf("Entre com o curso: ");
+        scanf("%s",aluno1.curso);
+        printf("Entre com o semestre: ");
+        scanf("%d",&aluno1.semestre);
+        printf("Entre com o dia de nascimento: ");
+        scanf("%d",&alunos.dia);
+        printf("Entre com o mes de nascimento: ");
+        scanf("%d",&alunos.mes);
+        printf("Entre com o ano de nascimento: ");
+        scanf("%d",&alunos.ano);
+        printf("Entre com o endereco: ");
+        scanf("%s",alunos.endereco);
+        printf("Entre com o email: ");
+        scanf("%s",alunos.email);
+        printf("Entre com o telefone de contato: ");
+        scanf("%s",alunos.telefone);
+        
+
+        /*Gravacaoo de dados no arquivo aluno.txt*/
+        fprintf(aluno,"Nome: %s  CPF: %s  Data de Nascimento:%d/%d/%d Endereco: %s Email: %s  Telefone: %s Numero de matricula: %s Curso: %s Semestre: %d \r\n",alunos.nome,alunos.cpf,alunos.dia,alunos.mes,alunos.ano,alunos.endereco, alunos.email,alunos.telefone,aluno1.matricula,aluno1.curso,aluno1.semestre);
+
+        /*Realizar um novo cadastro*/
+        printf("\nEntre com o nome (se não, digite FIM): ");
+        scanf("%s",alunos.nome);
+    }
+
+    /*Fechar o arquivo*/
+    fclose(aluno);
 
 }
 
 void CadastroProfessores(){
 
-    printf("\n\nAdicione os dados do Professor\n");
+    struct pessoa prof;
+    struct prof prof1;
+
+    /*Arquivos onde serÃ£o armazenados os dados*/
+    professor = fopen("professor.txt", "a+");
+
+    /*Realizar o cadastro do cliente*/
+
+    printf("Entre com o nome: ");
+    scanf("%s",prof.nome);
+    
+    while (strcmp(prof.nome,"FIM")){
+        
+        printf("Entre com o CPF: ");
+        scanf("%s",prof.cpf);
+        printf("Entre com o numero do siape: ");
+        scanf("%s",prof1.siape);
+        printf("Entre com o dia de nascimento: ");
+        scanf("%d",&prof.dia);
+        printf("Entre com o mes de nascimento: ");
+        scanf("%d",&prof.mes);
+        printf("Entre com o ano de nascimento: ");
+        scanf("%d",&prof.ano);
+        printf("Entre com o endereco: ");
+        scanf("%s",prof.endereco);
+        printf("Entre com o email: ");
+        scanf("%s",prof.email);
+        printf("Entre com o telefone de contato: ");
+        scanf("%s",prof.telefone);
+        printf("Entre com o status: ");
+        scanf("%d",&prof1.status);        
+
+        /*Gravacaoo de dados no arquivo professor.txt*/
+        fprintf(professor,"Nome: %s  CPF: %s  Data de Nascimento:%d/%d/%d Endereco: %s Email: %s  Telefone: %s Numero do Siape: %s Status: %d \r\n",prof.nome,prof.cpf,prof.dia,prof.mes,prof.ano,prof.endereco, prof.email,prof.telefone,prof1.siape,prof1.status);
+
+        /*Realizar um novo cadastro*/
+        printf("\nEntre com o nome (se não, digite FIM): ");
+        scanf("%s",prof.nome);
+    }
+
+    /*Fechar o arquivo*/
+    fclose(professor);
+
 
 }
 
-void CadastroProjeto(){
+void CadastroProjetos(){
 
     printf("\n\nAdicione os dados do TCC\n");
 
@@ -67,7 +174,7 @@ int main(){
 
     setlocale(LC_ALL, "");
 
-    FazerLogin();
+    TelaLogin();
 
     MostrarMenu();
 
@@ -75,22 +182,22 @@ int main(){
 
         {
 
-            case 1 : // Gravar Clientes
+            case 1 : // Gravar Alunos
 
                 CadastroAlunos();
 
             break;
 
-            case 2 : // Gravar Corretores
+            case 2 : // Gravar Professores
 
                 CadastroProfessores();
 
             break;
             
             case 3 : // Gravar TCC
-		
-		CadastroProjetos();
-			
+
+                CadastroProjetos();
+
             break;
 
             default :
